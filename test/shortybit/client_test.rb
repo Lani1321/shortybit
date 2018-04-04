b5946dabb452cbbbf34575e7c34a96cecaf45af1
Lani1321 <lani.berry13@gmail.com>
2018-04-04 03:50:15 -0400

    test #get_clicks returns an integer

--- test/shortybit/client_test.rb
+++ test/shortybit/client_test.rb
@@ -5,4 +5,14 @@ class ShortybitClientTest < Minitest::Test
     b = Shortybit::Client.new('234234324')
     assert_equal Shortybit::Client, b.class
   end
+
+  def test_get_clicks_returns_fixnum
+    VCR.use_cassette('get_click_count') do  
+      client = Shortybit::Client.new('664d80152194f99805a5979316e0f1af16416666')
+      client.get_clicks(client, "http://bit.ly/2GQBVJn")
+      assert_equal Shortybit::Client, client.class
+    end
+  end
+
+  
 end
